3e7faeb528793781e240e82633eb2c96f1c6375d
Eugene Mironyuk <me@don.ru>
2021-02-06 08:47:59 +0300

    initial

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+import { createApp } from 'vue'
+// TypeScript error? Run VSCode command
+// TypeScript: Select TypeScript version - > Use Workspace Version
+import App from './App.vue'
+
+createApp(App).mount('#app')
